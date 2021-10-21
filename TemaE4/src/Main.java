08318131e00d99d2e85d14fe5975c4fee10a0212
Andrei Nica <andreinyca@gmail.com>
2021-10-22 01:16:19 +0300

    Commit

---
+++ TemaE4/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[]args){
+        int a=-5;
+        int b=8;
+        int c=6;
+        float d=55;
+        float e=9;
+        float f=9; int g=20; float h=-3;float i=5; float j=8;int k=5; int l=15;int o=3;int m=2; int p=-8; int q=3;
+        System.out.println("a."+ "-5 + 8*6="+ (( b*c)+a));
+        System.out.println("b."+ "(55+9)%9="+ (( d+e)/f));
+        System.out.println("c."+ "(20+ -3*5/8="+ ( g+(h*i)/j));
+        System.out.println("d."+ "(5+15/3*2-8*3="+ ( g+(h*i)/j));
+    }
+
+}
